e781caef5f2bd51496175b5aea0daff57262e4c3
kp-void <contact@cody-p.me>
2019-10-17 10:14:42 -0700

    added default panic state

--- statemachine.c
+++ statemachine.c
@@ -1,5 +1,20 @@
 #include "statemachine.h"
 
 void state_machine(state_data* data) {
-	while (data->next_state) data->next_state(data);
+	while (data->next_state) {
+		state current_state = data->next_state;
+		data->next_state = panic_builtin;
+		current_state(data);
+	}
 }
+
+void panic_builtin(state_data * data) {
+	_STATE_CLEAR;
+	printf("Error: previous state did not specify next state!\n\n");
+	_STATE_PAUSE;
+	data->next_state = NULL;
+	data->return_code = INT_MAX;
+}
+
+#undef _STATE_CLEAR
+#undef _STATE_PAUSE
--- statemachine.h
+++ statemachine.h
@@ -3,23 +3,40 @@
 #ifndef STATEMACHINE_H
 #define STATEMACHINE_H
 
+#include <limits.h>
+#include <stdio.h>
+
 struct state_data_inner;
 
 typedef void (*state)(struct state_data_inner*);
 
-#ifdef STATEMACHINE_PARAMS
 typedef struct state_data_inner {
 	state next_state;
+	int return_code;
+	#ifdef STATEMACHINE_PARAMS
 	STATEMACHINE_PARAMS
+	#endif
 } state_data;
-#else
-typedef struct state_data_inner {
-	state next_state;
-} state_data;
-#endif
 
 
 // functions
 void state_machine(state_data * data);
+void panic_builtin(state_data * data);
+
+// ==========================================================================
+// OS DEPENDENT
+// ==========================================================================
+#if defined(_WIN32) || defined (_WIN64) // windows
+	#define _STATE_CLEAR system("cls")
+#elif defined (__linux__)
+	// TODO: change this to something more platform independent.
+	#define _STATE_CLEAR printf("\033[H\033[J")
+#elif defined (OS_OVERRIDE)
+
+#else
+	#error Unrecognized OS. Please find out how to define for this system.
+#endif
+
+#define _STATE_PAUSE printf("Press enter to continue."); for (int c; (c =getchar()) != '\n' && c != EOF;)
 
 #endif
